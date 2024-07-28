daad8de7d97c4690bc5fb10587fb5bd2468a2834
VadimP63 <pivovarov063@yandex.ru>
2024-07-28 19:36:47 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Task 1");
+
+
+        System.out.println("Task 2");
+        System.out.println("Task 3");
+        System.out.println("Task 4");
+        System.out.println("Task 5");
+        System.out.println("Task 6");
+        System.out.println("Task 7");
+        System.out.println("Task 8");
+        System.out.println("Task 9");
+        System.out.println("Task 10");
+    }
+}
